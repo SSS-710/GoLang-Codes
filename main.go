68dd6c99e06c6244b2ab80eb8f59a50020752daf
Shashank Singh Sikarwar <shashanksss710@gmail.com>
2026-06-09 23:03:39 +0530

    new updates

--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const LoginToken string = "shashank" // public
 
 func main() {
 	var username string = "sss"
 	fmt.Println("usrename")
 	fmt.Printf("Variable is of type: %T \n", username)
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type: %T \n", smallVal)
+
+	var smallFloat float32 = 255.5
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type: %T \n", smallFloat)
+
+	// default values and some aliases
+
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type: %T \n", anotherVariable)
+
+	var website = "learncodeonline.in"
+	fmt.Println(website)
+
+	// no var style
+
+	numberOfUser := 30000.0
+	fmt.Println(numberOfUser)
+
+	fmt.Println("LoginToken")
+	fmt.Printf("Variable is of type: %T \n", LoginToken)
+
 }
