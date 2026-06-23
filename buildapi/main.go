169f48df309aa5fb94c6d925b9975b362cecb2d9
Shashank Singh Sikarwar <shashanksss710@gmail.com>
2026-06-23 23:13:20 +0530

    delete controller fn

--- buildapi/main.go
+++ buildapi/main.go
@@ -96,3 +96,43 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 	return
 
 }
+
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create one course")
+	w.Header().Set("Content-type", "application/json")
+	params := mux.Vars(r)
+
+	// loop, id, remove, add with my ID
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], corses[index+1:]...)
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			course.CourseId = params["id"]
+			courses = append(courses, course)
+			json.NewEncoder(w).Encode(course)
+			return
+
+		}
+	}
+
+	// send a response when id is not found
+}
+func deleteOneCourse (w http.ResponseWriter, r *http.Request){
+	fmt.Println("Delete one course")
+	w.Header().Set("Content-Type","application/json")
+
+	params := mux.Vars(r)
+
+	// loop, id, remove(index, index+1)
+
+	for index, course := range courses{
+		if course.CourseId == params["id"]{
+			courses = append(courses,[:index], courses[index+1:]... )
+			break
+
+		}
+	}
+
+}
